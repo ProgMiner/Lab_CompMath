5ed910a83e209568dbbbd9429a4d2a4e297dfec9
Eridan Domoratskiy <eridan200@mail.ru>
2020-04-06 21:54:33 +0300

    Improve implementation of simple iterations method

--- src/main/kotlin/ru/byprogminer/compmath/lab3/math/SimpleIterationsMethod.kt
+++ src/main/kotlin/ru/byprogminer/compmath/lab3/math/SimpleIterationsMethod.kt
@@ -3,8 +3,11 @@ package ru.byprogminer.compmath.lab3.math
 import ru.byprogminer.compmath.lab3.equation.Equation
 import java.util.stream.Collectors
 import kotlin.math.abs
+import kotlin.math.max
 
 object SimpleIterationsMethod: EquationMethod {
+
+    private const val EPSILON = 1e-5
 
     override fun solve(equation: Equation, interval: Interval, precision: Precision): Set<Pair<Map<String, Double>, Int>> {
         if (equation.variables.isEmpty()) {
@@ -16,41 +19,33 @@ object SimpleIterationsMethod: EquationMethod {
         }
 
         val variable = equation.variables.first()
-        return equation.splitRoots(interval).map(selectApproximation(equation, variable))
-                .map { c -> simpleIteration(equation, precision, variable, c) }
-                .map { (x, i) -> mapOf(variable to x) to i }
-                .collect(Collectors.toSet())
-    }
+        return equation.splitRoots(interval).map { (begin, end) ->
+            val l = -1 / max(derivative(equation, variable, begin), derivative(equation, variable, end))
 
-    private fun selectApproximation(equation: Equation, variable: String) = { (begin, end): Pair<Double, Double> ->
-        selectApproximation(equation, variable, begin, end)
-    }
-
-    private fun selectApproximation(equation: Equation, variable: String, begin: Double, end: Double): Double {
-        val middle = begin + (end - begin) / 2
-
-        return listOf(
-                abs(equation.evaluateAsFunction(mapOf(variable to begin))) to begin,
-                abs(equation.evaluateAsFunction(mapOf(variable to middle))) to middle,
-                abs(equation.evaluateAsFunction(mapOf(variable to end))) to end
-        ).minBy { (f, _) -> f }!!.second
+            simpleIteration(equation, precision, variable, l, begin + (end - begin) / 2)
+        }.map { (x, i) -> mapOf(variable to x) to i }.collect(Collectors.toSet())
     }
 
     private tailrec fun simpleIteration(
             equation: Equation,
             precision: Precision,
             variable: String,
+            l: Double,
             c: Double,
             i: Int = 0
     ): Pair<Double, Int> {
-        val x = equation.evaluateAsFunction(mapOf(variable to c)) + c
+        val x = l * equation.evaluateAsFunction(mapOf(variable to c)) + c
 
         if (abs(x - c) < precision.precision || i >= precision.iterations) {
             return x to i
         }
 
-        return simpleIteration(equation, precision, variable, x, i + 1)
+        return simpleIteration(equation, precision, variable, l, x, i + 1)
     }
+
+    private fun derivative(equation: Equation, variable: String, x: Double) =
+            (equation.evaluateAsFunction(mapOf(variable to x + EPSILON)) -
+                    equation.evaluateAsFunction(mapOf(variable to x))) / EPSILON
 
     override fun toString() = "Simple iterations method"
 }
